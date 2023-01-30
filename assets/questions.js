4ee6fc605e59bce88e99bc09e1c8ce8e6a0345ec
pattersonhn <pattersonhn01@gmail.com>
2023-01-30 14:00:07 -0600

    Added functionality to JS and updated HTML/CSS

---
+++ assets/questions.js
@@ -0,0 +1,67 @@
+// creating an array and passing the number, questions, options, and answers
+var questions = [
+    {
+      title: "Commonly used data types do NOT include:",
+      choices: [
+      "Strings",
+      "Booleans",
+      "Alerts",
+      "Numbers"
+    ],
+      answer: "Alerts"
+  },
+    {
+      title: "The condition in an if/else statement is enclosed with ______.",
+      choices: [
+      "Quotes",
+      "Curly brackets",
+      "Parenthesis",
+      "Square brackets"
+    ],
+      answer: "Parenthesis"
+  },
+    {
+      title: "Arrays in JavaScript can be used to store ______.",
+      choices: [
+      "Numbers and strings",
+      "Other arrays",
+      "Booleans",
+      "All of the above"
+    ],
+      answer: "All of the above",
+  },
+    {
+      title: "String values must be enclosed within ______ when being assigned to variables.",
+      choices: [
+      "Commas",
+      "Curly brackets",
+      "Quotes",
+      "Parenthesis"
+    ],
+      answer: "Quotes",
+  },
+    {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: [
+      "JavaScript",
+      "Terminal/Bash",
+      "For loops",
+      "console.log"
+    ],
+      answer: "console.log",
+  }
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
